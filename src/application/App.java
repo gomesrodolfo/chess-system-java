0e9133e9af80a6e221e29008232a9fcabbd85dc4
Rodolfo Santos Gomes <40841803+gomesrodolfo@users.noreply.github.com>
2022-03-04 11:36:42 -0300

    Primeira classe ChessPosition

--- src/application/App.java
+++ src/application/App.java
@@ -1,7 +1,11 @@
 package application;
 
+import boardgame.ChessPosition;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        ChessPosition pos = new ChessPosition(3, 5);
+        System.out.println(pos);
     }
 }
